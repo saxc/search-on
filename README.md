a298e111f18473535bb89e066d29983acbea9cac
saxc <saxc@users.noreply.github.com>
2025-10-11 00:15:43 +0200

    inti commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# Search On ...
+**Search On ...** is an editor extension that allows developers to easily search selected text across multiple platforms. With this extension, you can quickly access information from websites like Google, GitHub, MDN, and many others.
+
+## Included platforms
+- CSS-Tricks
+- GitHub
+- Google
+- MDN (Mozilla Developer Network)
+- NPM (Node Package Manager)
+- Reddit
+- Stack Overflow
+- Wikipedia
+- YouTube
+
+## Settings
+
+### Platforms
+- All included platforms can be enabled/disabled in the settings.
+
+### Custom Platforms
+-You can add your own platforms in the settings.
+
+```json
+"searchOn.customPlatforms": [
+    { "label": "Amazon", "url": "https://www.amazon.com/s?k="},
+    { "label": "eBay", "url": "https://www.ebay.com/sch/i.html?_nkw="}
+]
+```
---
+++ src/extension.ts
@@ -0,0 +1,119 @@
+import * as vscode from "vscode";
+
+export function activate(context: vscode.ExtensionContext) {
+  context.subscriptions.push(
+    vscode.commands.registerCommand("search-on.search", async () => {
+      await searchOn();
+    })
+  );
+}
+
+export function deactivate() {}
+
+interface Platform {
+  label: string;
+  url: string;
+}
+
+async function searchOn() {
+  const editor = vscode.window.activeTextEditor;
+  if (editor) {
+    const selectedText = editor.document.getText(editor.selection);
+
+    let platforms: Platform[] = [];
+
+    platforms.push(...getSearchPlatforms());
+    platforms.push(...getCustomPlatforms());
+
+    platforms = sortPlatformsByLabel(platforms);
+
+    const pick = await vscode.window.showQuickPick(platforms, {
+      placeHolder: "Select a search platform",
+    });
+    if (pick) {
+      const searchUrl = pick.url + encodeURIComponent(selectedText);
+      vscode.env.openExternal(vscode.Uri.parse(searchUrl));
+    }
+  }
+}
+
+function getSearchPlatforms(): Platform[] {
+  let platforms: Platform[] = [];
+
+  const config = vscode.workspace.getConfiguration("searchOn");
+
+  if (config.get<boolean>("google")) {
+    platforms.push({
+      label: "Google",
+      url: "https://www.google.com/search?q=",
+    });
+  }
+
+  if (config.get<boolean>("gitHub")) {
+    platforms.push({ label: "GitHub", url: "https://github.com/search?q=" });
+  }
+
+  if (config.get<boolean>("MDN")) {
+    platforms.push({
+      label: "MDN",
+      url: "https://developer.mozilla.org/en-US/search?q=",
+    });
+  }
+
+  if (config.get<boolean>("reddit")) {
+    platforms.push({
+      label: "Reddit",
+      url: "https://www.reddit.com/search?q=",
+    });
+  }
+
+  if (config.get<boolean>("stackOverflow")) {
+    platforms.push({
+      label: "Stack Overflow",
+      url: "https://stackoverflow.com/search?q=",
+    });
+  }
+
+  if (config.get<boolean>("wikipedia")) {
+    platforms.push({
+      label: "Wikipedia",
+      url: "https://de.wikipedia.org/w/index.php?search=",
+    });
+  }
+
+  if (config.get<boolean>("youTube")) {
+    platforms.push({
+      label: "YouTube",
+      url: "https://www.youtube.com/results?search_query=",
+    });
+  }
+
+  if (config.get<boolean>("NPM")) {
+    platforms.push({ label: "NPM", url: "https://www.npmjs.com/search?q=" });
+  }
+
+  if (config.get<boolean>("cssTricks")) {
+    platforms.push({ label: "CSS-Tricks", url: "https://css-tricks.com/?s=" });
+  }
+
+  return platforms;
+}
+
+function sortPlatformsByLabel(platforms: Platform[]): Platform[] {
+  return platforms.sort((a, b) => {
+    if (a.label < b.label) {
+      return -1;
+    }
+    if (a.label > b.label) {
+      return 1;
+    }
+    return 0;
+  });
+}
+
+function getCustomPlatforms(): Platform[] {
+  const config = vscode.workspace.getConfiguration("searchOn");
+  const customPlatforms: Platform[] =
+    config.get<Platform[]>("customPlatforms") || [];
+  return customPlatforms;
+}
